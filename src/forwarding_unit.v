f09990f911b53305bfb0d368c0667deb1b4a983c
Rishith-V <rishithvulli18@gmail.com>
2026-04-19 17:21:53 +0530

    modified forwarding unit

--- src/forwarding_unit.v
+++ src/forwarding_unit.v
@@ -5,14 +5,11 @@ module forwarding_unit(rs1_ex, rs2_ex, rd_mem, rd_wb, regw_mem, regw_wb, forward
     
     output [1:0] forward_a, forward_b;
 
-    // wire forward_a_mem, forward_b_mem;
-    // wire forward_a_wb, forward_b_wb;
-
-    assign forward_a = (rs1_ex == rd_mem && regw_mem && rd_mem != 5'b0) ? 2'b01 : 
+    assign forward_a = (rs1_ex == rd_mem && regw_mem && rd_mem != 5'b0) ? 2'b01 :   //for rs1
                         (rs1_ex == rd_wb && regw_wb && rd_wb != 5'b0) ? 2'b10 : 
                         2'b00;
     
-    assign forward_b = (rs2_ex == rd_mem && regw_mem && rd_mem != 5'b0) ? 2'b01 : 
+    assign forward_b = (rs2_ex == rd_mem && regw_mem && rd_mem != 5'b0) ? 2'b01 :   //for rs2
                         (rs2_ex == rd_wb && regw_wb && rd_wb != 5'b0) ? 2'b10 : 
                         2'b00;
 
